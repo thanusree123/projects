4e024c0a775bafa0c8aa86d53ac76dd6b22eb839
thanusree123 <gollathanusree0@gmail.com>
2026-06-25 21:44:04 +0530

    node

--- student-manager/app.js
+++ student-manager/app.js
@@ -1,3 +1,27 @@
-const studentServer=require("./studentServer")
-studentServer.showStudent();
-studentServer.addStudent("D",70);
+const choice = process.argv[2];
+const name = process.argv[3];
+const marks = Number(process.argv[4]);
+switch(choice){
+    case "show":
+        studentServer.showStudent();
+        break;
+
+    case "add":
+        studentServer.addStudent(name, marks);
+        break;
+
+    case "find":
+        console.log(studentServer.findStudent(name));
+        break;
+
+    case "delete":
+        studentServer.deleteStudent(name);
+        break;
+
+    case "update":
+        studentServer.updateMarks(name, marks);
+        break;
+
+    default:
+        console.log("Invalid command");
+}
--- student-manager/studentServer.js
+++ student-manager/studentServer.js
@@ -1,29 +1,108 @@
 const fs=require("fs")
-
 function getStudent(){
-    const data=fs.readFileSync(
-        "students.json",
-        "utf8"
-    );
-    return JSON.parse(data);
+    try{
+        const students = JSON.parse(
+            fs.readFileSync("students.json")
+        );
+        return students;
+    }
+    catch(error){
+        console.log("Error reading students file");
+        return [];
+    }
 }
 function showStudent(){
+    try{
     const students=getStudent();
     console.log(students)
+    }
+    catch(error){
+        console.log("error showing students");
+    }
 }
-function addStudent(name,marks){
-    const student=getStudent()
-    student.push({
+function addStudent(name, marks){
+    try{
+    const students = getStudent();
+    const existingStudent =
+        students.find(student => student.name === name);
+    if(existingStudent){
+        console.log("Student already exists");
+        return;
+    }
+    students.push({
         name,
         marks
     });
+
     fs.writeFileSync(
         "students.json",
-        JSON.stringify(student)
-    )
+        JSON.stringify(students)
+    );
+    console.log("student added successfully");
 }
+catch(error){
+    console.log("error adding a student");
+}
+}
+function findStudent(name){
+    try{
+    let students =JSON.parse(fs.readFileSync("students.json"));
+    for(let student of students){
+        if(student.name===name){
+            return student;
+        }
+    }
+    return null;
+}
+catch(error){
+    console.log("error finding student");
+    return null;
+}
+}
+function deleteStudent(name){
+    try{
+    const students=getStudent();
+    const updateStudent=students.filter(
+        student=>student.name!==name
+    );
+    fs.writeFileSync(
+        "students.json",
+        JSON.stringify(updateStudent)
+    );
+    console.log("student deleted successfully")
+}
+catch(error){
+    console.log("error deleting student");
+}
+}
+function updateMarks(name, newMarks){
+    try{
+        const students = getStudent();
 
+        for(let student of students){
+            if(student.name === name){
+                student.marks = newMarks;
+
+                fs.writeFileSync(
+                    "students.json",
+                    JSON.stringify(students)
+                );
+
+                console.log("Marks updated successfully");
+                return;
+            }
+        }
+
+        console.log("Student not found");
+    }
+    catch(error){
+        console.log("Error updating marks");
+    }
+}
 module.exports={
     showStudent,
-    addStudent
+    addStudent,
+    findStudent,
+    deleteStudent,
+    updateMarks
 }
